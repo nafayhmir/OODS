53b120f11878f7eaf189694127e8f71b8576276a
Muhammad Nafayh Akhlaq <nafayhmir@gmail.com>
2026-08-18 23:00:04 +0500

    Created Class for Books

---
+++ OOD Lab/Lab_1/Lab1_Task1.cpp
@@ -0,0 +1,51 @@
+//Lab 1, 25i-6180
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class book{
+    private:
+    string title;
+    string author;
+    double price;
+
+    public:
+    book() //Default Constructor
+    {
+        title=" ";
+        author=" ";
+        price=0;
+    }
+    book(string a,string b, double c) //Parameterized Constructor
+    {
+        title=a;
+        author=b;
+        price=c;
+    }
+    void sett(string a) //Set and Get Functions
+    {
+        title=a;
+    }
+    void seta(string b)
+    {
+        author=b;
+    }
+    void setp(double c)
+    {
+        price=c;
+    }
+    string gett()
+    {
+        return title;
+    } 
+    string geta()
+    {
+        return author;
+    } 
+    double getp()
+    {
+        return price;
+    } 
+};
